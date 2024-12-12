667f48a207277b5b25653856ff41299387be2c74
rcalcagniti <rcalcagniti@gmail.com>
2024-12-12 19:04:46 -0300

    começo projeto conversor

---
+++ JavaScript/Projeto Conversor/conversor.js
@@ -0,0 +1,11 @@
+let qualquerCoisa = 0;
+console.log (qualquerCoisa);
+
+function aparecerElemento () {
+    let selectConversorJS = document.getElementById("selectConversor").value;
+    console.log(selectConversorJS);
+};
+
+function converterTemperatura () {};
+
+function converterPeso () {};
